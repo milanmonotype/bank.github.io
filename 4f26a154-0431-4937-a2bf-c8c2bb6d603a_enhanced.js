12281775dd147d0cb8d517b6d7ed944d73f5371e
Milan Amatya <134918466+milanmonotype@users.noreply.github.com>
2025-04-23 11:53:30 +0000

    khoya

---
+++ 4f26a154-0431-4937-a2bf-c8c2bb6d603a_enhanced.js
@@ -0,0 +1,8 @@
+var MTIProjectId='4f26a154-0431-4937-a2bf-c8c2bb6d603a';
+ (function() {
+        var mtiTracking = document.createElement('script');
+        mtiTracking.type='text/javascript';
+        mtiTracking.async='true';
+        mtiTracking.src=('https:'==document.location.protocol?'https:':'http:')+'//cdn.fonts.net/t/trackingCode.js';
+        (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild( mtiTracking );
+   })();
